2d50a4f75ee7a7368dc9ccf74fc0767fdb0fead4
laurent.tardif2010 <laurent.tardif2010@6d516167-9d04-95c1-81e7-fc8134dbc2da>
2010-09-27 09:20:18 +0000

    git-svn-id: https://agilegrenoble2010-4techos.googlecode.com/svn/trunk@71 6d516167-9d04-95c1-81e7-fc8134dbc2da

--- Installation/install.bat
+++ Installation/install.bat
@@ -1,13 +1,33 @@
 rem Installation script for the build platform
 echo "Create destination dir"
-mkdir e:\tools\wget
-echo "copying needed tools"
-copy unzip.exe e:\tools
-copy wget-1.11.4-1-bin.zip e:\tools
-echo "going to the tools directory"
+
+rem ***************************************************************
+rem INSTALLATION BASIC STUFF 
+rem ***************************************************************
+
+rem mkdir e:\tools\wget
+rem echo "copying needed tools"
+rem copy unzip.exe e:\tools
+rem copy wget-1.11.4-1-bin.zip e:\tools
+rem copy wget-1.11.4-1-dep.zip e:\tools
+rem echo "going to the tools directory"
 e:
-cd e:\tools\wget
-echo "starting installation"
-e:\tools\unzip.exe ..\wget-1.11.4-1-bin.zip
+rem cd e:\tools\wget
+rem echo "starting installation"
+rem e:\tools\unzip.exe ..\wget-1.11.4-1-bin.zip
+rem e:\tools\unzip.exe ..\wget-1.11.4-1-dep.zip
 cd e:\tools
-e:\tools\wget\bin\wget.exe "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-i586.exe?BundledLineItemUUID=hNyJ_hCvW_UAAAErF6IpGFJf&OrderID=P1OJ_hCvVSEAAAErDKIpGFJf&ProductID=h2iJ_hCyKS4AAAEpVrwzBGsB&FileName=/jdk-6u21-windows-i586.exe"
+
+rem ***************************************************************
+rem INSTALLATION JDK
+rem ***************************************************************
+rem e:\tools\wget\bin\wget.exe "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-i586.exe?BundledLineItemUUID=hNyJ_hCvW_UAAAErF6IpGFJf&OrderID=P1OJ_hCvVSEAAAErDKIpGFJf&ProductID=h2iJ_hCyKS4AAAEpVrwzBGsB&FileName=/jdk-6u21-windows-i586.exe"
+rem move "jdk-6u21-windows-i586*" "jdk-6u21-windows-i586.exe"
+rem  "jdk-6u21-windows-i586.exe" /l* java.log INSTALLDIR=e:\java\jdk1.6.0_21 /qn
+
+
+rem ***************************************************************
+rem INSTALLATION MYSQL
+rem ***************************************************************
+rem e:\tools\wget\bin\wget.exe 
+msiexec /i mysql-5.1.50-win32.msi /l* mysql.log INSTALLDIR="E:\MySQL\Server 5.1\" DATADIR="E:\data\MySQL\" /qn
