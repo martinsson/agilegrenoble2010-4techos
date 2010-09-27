8bfccbca26d0dd0383ceddb1e3d29044c247bcb6
laurent.tardif2010 <laurent.tardif2010@6d516167-9d04-95c1-81e7-fc8134dbc2da>
2010-09-27 08:51:28 +0000

    git-svn-id: https://agilegrenoble2010-4techos.googlecode.com/svn/trunk@70 6d516167-9d04-95c1-81e7-fc8134dbc2da

--- Installation/install.bat
+++ Installation/install.bat
@@ -1,11 +1,13 @@
 rem Installation script for the build platform
 echo "Create destination dir"
-mkdir -p e:\tools\wget
+mkdir e:\tools\wget
 echo "copying needed tools"
 copy unzip.exe e:\tools
 copy wget-1.11.4-1-bin.zip e:\tools
 echo "going to the tools directory"
-cd e:\tools\wget
 e:
-"echo starting installation"
+cd e:\tools\wget
+echo "starting installation"
 e:\tools\unzip.exe ..\wget-1.11.4-1-bin.zip
+cd e:\tools
+e:\tools\wget\bin\wget.exe "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-i586.exe?BundledLineItemUUID=hNyJ_hCvW_UAAAErF6IpGFJf&OrderID=P1OJ_hCvVSEAAAErDKIpGFJf&ProductID=h2iJ_hCyKS4AAAEpVrwzBGsB&FileName=/jdk-6u21-windows-i586.exe"
