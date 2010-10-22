c1448ebdf092c7ca3628718c958f147288506837
cyrille.roy@free.fr <cyrille.roy@free.fr@6d516167-9d04-95c1-81e7-fc8134dbc2da>
2010-10-22 18:55:34 +0000

    adding shell script to install xmlbean 2.5.0
    git-svn-id: https://agilegrenoble2010-4techos.googlecode.com/svn/trunk@148 6d516167-9d04-95c1-81e7-fc8134dbc2da

---
+++ Installation/installPartial.sh
@@ -0,0 +1,10 @@
+workspaceDir=/home/cyrille/workspace
+
+mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xbeans -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xbean.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXbeans.xml
+
+mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=jsr-api -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/jsr173_1.0_api.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomJsrAPI.xml
+mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=resolver -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/resolver.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomResolver.xml
+mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xbeans-xpath -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xbean_xpath.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXbeansXPath.xml
+mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xbeans-qname -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xmlbeans-qname.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXmlbeans-qname.xml
+mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xml-public -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xmlpublic.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXmlPublic.xml
+
