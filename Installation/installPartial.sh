cc8445d9a3d2c806b99dee39d2e397beefddf462
laurent.tardif2010 <laurent.tardif2010@6d516167-9d04-95c1-81e7-fc8134dbc2da>
2010-10-25 11:24:26 +0000

    LT : commentaire pour cyrille :-D
    git-svn-id: https://agilegrenoble2010-4techos.googlecode.com/svn/trunk@149 6d516167-9d04-95c1-81e7-fc8134dbc2da

--- Installation/installPartial.sh
+++ Installation/installPartial.sh
@@ -1,4 +1,8 @@
+# LTA : tu pourrais au moins prendre un parametre a la ligne de commande :-)
+# gougnafier :)
+
 workspaceDir=/home/cyrille/workspace
+
 
 mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xbeans -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xbean.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXbeans.xml
 
