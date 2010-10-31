61227ebad937866a0ec81cdcee77a379d3f7ab0d
Johan Martinsson <jm1974@hotmail.com>
2010-10-31 11:24:33 +0100

    merged in teamTDD

--- Installation/installPartial.sh
+++ Installation/installPartial.sh
@@ -7,4 +7,4 @@ mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=resolver -Dv
 mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xbeans-xpath -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xbean_xpath.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXbeansXPath.xml
 mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xbeans-qname -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xmlbeans-qname.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXmlbeans-qname.xml
 mvn install:install-file -DgroupId=org.apache.xmlbeans -DartifactId=xml-public -Dversion=2.5.0 -Dpackaging=jar -Dfile=$workspaceDir/ThirdParty/xmlBeans/lib/xmlpublic.jar -DpomFile=$workspaceDir/ThirdParty/poms/pomXmlPublic.xml
-
+mvn install:install-file -DgroupId=org.agile4techos.nokiatest -DartifactId=questionType -Dpackaging=jar -Dfile=$workspaceDir/QuestionsType/bin/questionType-1.0.0.jar -DpomFile=$workspaceDir/QuestionsType/pom.xml
