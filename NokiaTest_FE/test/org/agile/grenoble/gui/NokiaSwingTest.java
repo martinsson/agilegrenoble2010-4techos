236a7f9f6f3535d3ae154b2f0f6f3ec8bf975e19
Johan Martinsson <jm1974@hotmail.com>
2010-11-12 23:27:46 +0100

    got rid of all those horrible commmented lines

--- NokiaTest_FE/test/org/agile/grenoble/gui/NokiaSwingTest.java
+++ NokiaTest_FE/test/org/agile/grenoble/gui/NokiaSwingTest.java
@@ -29,41 +29,5 @@ public class NokiaSwingTest {
 		assertThat(questionsPanel.length, equalTo(7));
 	}
 	
-//	@Test
-//	public void addScorePanelToQuestionsPanels() throws Exception {
-//		NokiaSwing nokiaSwing = new NokiaSwing();
-//		JPanel[] questionsPanel = nokiaSwing.generateQuestionsPanels(questionsType);
-//		assertThat(questionsPanel.length, equalTo(8));
-//		JPanel scorePanel = questionsPanel[7];
-//
-//		assertThat(scorePanel, instanceOf(QuestionPanel.class));
-//		MatcherAssert.assertThat(scorePanel, hasTextField(startsWith("NokiaTest result is")));
-//		
-//	}
-//	
-//    private Matcher<Component[]> hasTextComponent(
-//			Matcher<JTextComponent> matcher) {
-////    	return new TypeSafeDiagnosingMatcher<Component>() {
-////		};
-//		return (Matcher<Component[]>)hasItemInArray(allOf(instanceOf(JTextComponent.class), matcher));
-//	}
-//
-//	public Matcher<JTextComponent> withText(final Matcher<String> matcher) {
-//		return new TypeSafeDiagnosingMatcher<JTextComponent>() {
-//
-//			public void describeTo(Description description) {
-//				description.appendText("a text panel with ").appendDescriptionOf(matcher);
-//			}
-//
-//			@Override
-//			protected boolean matchesSafely(JTextComponent item, Description mismatchDescription) {
-//				mismatchDescription.appendText("toto");
-//				return matcher.matches(item.getText());
-//			}
-//		};
-//		
-//    }
-    
-	
 
 }
